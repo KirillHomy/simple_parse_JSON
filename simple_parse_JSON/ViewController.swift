f6df7a79e59e533b95ef717a419f251dc1b909ac
Kirill Khomicevich <khomicevichkirill@icloud.com>
2023-04-15 17:26:19 +0300

    Initial Commit

---
+++ simple_parse_JSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_parse_JSON
+//
+//  Created by Kirill Khomytsevych on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
